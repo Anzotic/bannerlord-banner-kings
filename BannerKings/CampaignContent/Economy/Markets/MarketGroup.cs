3148caa06714062e919310b0a041cb1a7a0427d2
Anzotic <Anzotic@hotmail.com>
2025-08-30 11:22:48 -0400

    fix: add missing GetDemand method and Demands field in MarketGroup

--- BannerKings/CampaignContent/Economy/Markets/MarketGroup.cs
+++ BannerKings/CampaignContent/Economy/Markets/MarketGroup.cs
@@ -1,31 +1,44 @@
 ﻿using System.Collections.Generic;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
 using TaleWorlds.Localization;
 
 namespace BannerKings.CampaignContent.Economy.Markets
 {
     public class MarketGroup : BannerKingsObject
     {
-        public MarketGroup(string id) : base(id)
-        {
+        public MarketGroup(string id) : base(id) {
         }
 
-        public void Initialize(TextObject name, TextObject description, CultureObject culture, Dictionary<CultureObject, float> spawns)
-        {
+        public void Initialize(TextObject name, TextObject description, CultureObject culture, Dictionary<CultureObject, float> spawns, Dictionary<ItemCategory, float> demands = null) {
             Initialize(name, description);
             Culture = culture;
             Spawns = spawns;
+            Demands = demands;
         }
 
         public CultureObject Culture { get; private set; }
         public Dictionary<CultureObject, float> Spawns { get; private set; }
+        public Dictionary<ItemCategory, float> Demands { get; private set; }
 
-        public float GetSpawn(CultureObject culture)
-        {
+        public float GetSpawn(CultureObject culture) {
             float result = 0f;
             if (culture.StringId == Culture.StringId) result = 1f;
             else if (Spawns.ContainsKey(culture)) result = Spawns[culture];
 
+            return result;
+        }
+
+        public float GetDemand(ItemCategory itemCategory) {
+            bool flag = this.Demands == null;
+            if (flag) {
+                this.Demands = new Dictionary<ItemCategory, float>();
+            }
+            float result;
+            bool flag2 = !this.Demands.TryGetValue(itemCategory, out result);
+            if (flag2) {
+                result = 1f;
+            }
             return result;
         }
     }
